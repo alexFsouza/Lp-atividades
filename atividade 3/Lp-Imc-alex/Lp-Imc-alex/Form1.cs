3f872d22c1b9be22db7db3c1267650e50425282a
alex <99513008+alexFsouza@users.noreply.github.com>
2022-04-08 16:47:39 -0300

    Add files via upload

---
+++ atividade 3/Lp-Imc-alex/Lp-Imc-alex/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lp_Imc_alex
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double teste =0;
+            string peso = txtPeso.Text;
+            string altura = txtAltura.Text;
+
+
+            if (!double.TryParse(peso, out teste))
+            {
+                MessageBox.Show("Digite um valor numérico!");
+            }
+            else if (!double.TryParse(altura, out teste))
+            {
+                MessageBox.Show("Digite um valor numérico");
+            }
+            else
+            {
+                double peso1 = Convert.ToDouble(peso);
+
+                double altura1 = Convert.ToDouble(altura);
+
+                double imc = peso1 / (altura1 * altura1);
+
+
+                if (imc < 18.5)
+                {
+                    MessageBox.Show(imc + ": Classificação: Magreza, Obsidade Grau: 0");
+
+                }
+                else if (imc >= 18.5 & imc < 24.9)
+                {
+                    MessageBox.Show(imc + ": Classificação: Normal, Obsidade Grau: 0");
+                }
+                else if (imc >= 25 & imc < 29.9)
+                {
+                    MessageBox.Show(imc + ": Classificação: Sobre Peso, Obsidade Grau: 1" );
+                }
+                else if (imc >= 30 & imc < 39.9)
+                {
+                    MessageBox.Show(imc + ": Classificação: Obesidade, Obsidade Grau: 2");
+                }
+                else
+                {
+                    MessageBox.Show(imc + ": Classificação: Obesidade  Grave, Obsidade Grau: 3");
+                }
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+
+        }
+    }
+}
